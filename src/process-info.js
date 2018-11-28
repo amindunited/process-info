8b62968316fbb03c3cd29659abdb953305e0fbcd
Robin Buckley <robin.buckley@auspost.com.au>
2018-11-28 12:45:14 +1100

    Convert from class to function

--- src/process-info.js
+++ src/process-info.js
@@ -9,29 +9,49 @@
 const ROOT_DIR = process.cwd();
 const _package = require(ROOT_DIR + '/package.json');
 
+const argObject = {
+  package: _package
+};
+
 /**
  * Gets and provides both the command line arguments,
  * and the package.json
  */
 
-class ProcessInfo {
-  constructor () {
-    this.package = _package;
-    this.getArgs();
-  }
+const processInfo = () => {
 
-  getArgs () {
-    //The first 2 objects in the array are the env, and script...we don't need those
-    let args = process.argv.splice(2);
+  //The first 2 objects in the array are the env, and script...we don't need those
+  let args = process.argv.splice(2);
 
-    //major, minor, buildNumber
-    args.forEach((obj) => {
-      let keyVal = obj.split('=');
-      let key = keyVal[0].replace('--', '');
-      let val = keyVal[1];
-      this[key] = val;
-    });
-  }
+  // argument names must begin with two dashes: --[name]
+  // BUT
+  // the values can be assigned
+  // --[name]=[value]
+  // or
+  // --[name] [value]
+
+  const reducedArgs = args.reduce((accumulator, currentValue) => {
+
+    if (currentValue.match(/^--/)) {
+
+      let value = currentValue.match('=') ? currentValue.split('=')[1] : true;
+      argObject[currentValue.replace(/^--/, '')] = value;
+      // We only need to save the keys
+      accumulator.push(currentValue);
+
+    } else {
+
+      const lastIndex = accumulator.length - 1;
+      const lastKey = accumulator[lastIndex];
+      argObject[lastKey] = currentValue;
+
+    }
+    return accumulator;
+
+  }, []);
+
+  return argObject;
+
 }
 
-module.exports = new ProcessInfo();
+module.exports = processInfo();
